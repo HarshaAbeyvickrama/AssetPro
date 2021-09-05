f747e6f75d7d44a635a0b6878c685572cd042a15
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2021-09-05 18:01:57 +0530

    added left nav redirection

--- controller/mainController.php
+++ controller/mainController.php
@@ -1,14 +1,18 @@
 <?php
-    if(isset($_GET['action']) || isset($_GET['view'])){
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+    if(isset($_GET['action'])){
         require_once("loginController.php");
         switch ($_GET['action']) {
             case 'renderView':
-               echo renderView();
+               echo renderView($_GET['view']);
                 break;
+
             case 'logout':
-                
                 logOut();
                 break;
+
             case 'login':
                 login($_POST["Username"],$_POST["Password"]);
                 break;
@@ -19,7 +23,34 @@
         }
     }
 
-    function renderView(){
-        return include("../view/includes/profile.php");
+    function renderView($view){
+
+        if($view == "profile"){
+            return include("../view/includes/profile.php");
+        }
+
+        switch ($_SESSION['userType']) {
+            case 'admin':
+                $location = "../view/includes/admin/";
+                break;
+
+            case 'assetManager':
+                $location = "../view/includes/assetManager/";
+                break;
+
+            case 'employee':
+                $location = "../view/includes/employee/";
+                break;
+
+            case 'technician':
+                $location = "../view/includes/technician/";
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+
+        return include($location.$view.".php");
     }
 ?>
