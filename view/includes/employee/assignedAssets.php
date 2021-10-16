9c05ea73e28d2af700d649b5bbb9778f776750b3
Mushrifa <mushimmf7877@gmail.com>
2021-10-16 21:15:16 +0530

    modified employeeass,over,rep

--- view/includes/employee/assignedAssets.php
+++ view/includes/employee/assignedAssets.php
@@ -8,23 +8,19 @@
         padding: 20px;
         background-color: #F1F4FF;
     }
-
     .overviewLayout>div {
         display: flex;
         align-items: center;
         color: #304068;
         font-size: 24px;
         font-weight: bold;
-
     }
-
     .statSection {
         display: grid;
         grid-template-columns: repeat(5, 1fr);
         width: 100%;
         height: 100%;
     }
-
     .statSection>div {
         width: 100%;
         height: 100%;
@@ -32,7 +28,6 @@
         /* justify-content: center; */
         align-items: center;
     }
-
     .statBox {
         display: grid;
         grid-template-rows: 3fr 2fr;
@@ -41,42 +36,33 @@
         width: 90%;
         border-radius: 12px;
     }
-
     .statBox>div {
         display: flex;
         justify-content: center;
         align-items: center;
     }
-
     .statNumber {
         font-size: 40px;
     }
-
     .statText {
         font-size: 17px;
         font-weight: lighter;
     }
-
     .box1 {
         background-color: #304068;
     }
-
     .box2 {
         background-color: #6A71D7;
     }
-
     .box3 {
         background-color: #3D7DDB;
     }
-
     .box4 {
         background-color: #6165A2;
     }
-
     .box5 {
         background-color: #4E74AB;
     }
-
     .contentSection {
         background-color: white;
         border-radius: 15px;
@@ -84,16 +70,13 @@
         width: 100%;
         height: 100%;
     }
-
     /*inside content section*/
     .econtainer1 {
         width: 100%;
         height: 100%;
         display: grid;
         grid-template-columns: 2fr 14fr 2fr 1fr;
-
     }
-
     #reportbtn {
         color: white;
         background-color: #5C6E9B;
@@ -105,7 +88,6 @@
         cursor: pointer;
         font-size: 15px;
     }
-
     /*------------------------------------------modified one ----------------------------------*/
     .table {
         display: table;
@@ -113,10 +95,7 @@
         margin: 10px 0px;
         color: #5C6E9B;
         overflow-y: hidden !important;
-
-
     }
-
     .tableHeader {
         width: 100%;
         display: table-header-group;
@@ -124,51 +103,38 @@
         padding: 15px;
         font-weight: bold;
         overflow-y: hidden !important;
-
     }
-
     .tableHeader>div {
         display: table-cell;
     }
-
     .table .tableRowGroup {
         display: table-row-group;
         overflow-y: auto !important;
-
     }
-
     .tableRow {
         display: table-row;
     }
-
     .tableCell {
         display: table-cell;
     }
-
     .tableRowGroup .tableRow:hover {
         cursor: pointer;
         background-color: wheat;
     }
-
     /* Table overflow */
     .tableRowGroup {
         overflow-y: auto;
     }
-
     .tableRow .tableCell {
         padding: 10px 0px;
-
     }
-
     .tableRow>div {
         display: table-cell;
         padding: 10px 0px;
     }
-
     .tableRow>div:first-of-type {
         text-align: center;
     }
-
     .tableHeader>div:first-of-type {
         text-align: center;
     }
@@ -186,42 +152,34 @@
                 <div class="statText">All Assets</div>
             </div>
         </div>
-
         <div>
             <div class="statBox box2" id="allEmployees">
                 <div class="statNumber">70</div>
                 <div class="statText">Tangible Assets</div>
             </div>
         </div>
-
         <div>
             <div class="statBox box3" id="allTechnicians">
                 <div class="statNumber">40</div>
                 <div class="statText">Fixed Assets</div>
             </div>
         </div>
-
         <div>
             <div class="statBox box4" id="allAssets">
                 <div class="statNumber">30</div>
                 <div class="statText">Consumable Assets</div>
             </div>
         </div>
-
         <div>
             <div class="statBox box5" id="allAssets">
                 <div class="statNumber">30</div>
                 <div class="statText">Intangible Assets</div>
             </div>
         </div>
-
-
     </div>
-
     <div>
         <div>Recent Activities</div>
     </div>
-
     <div class="contentSection">
         <!-- <div class="econtainer1">  -->
         <div class="table">
@@ -232,12 +190,7 @@
                 <div>Asset Type</div>
                 <div>ReportBreakdown</div>
             </div>
-
-
             <div class="tableRowGroup" id="allAssetsEmp">
-
-
-
             </div>
         </div>
     </div>
@@ -247,11 +200,9 @@
 
 
 <script>
-
    function loadAssets(){
         const xhr = new XMLHttpRequest();
         xhr.open("GET", "../model/AssignedAssetsEmp.php?action=getAssets", true);
-
         xhr.onload = function() {
             if (this.status === 200) {
                 var assets = JSON.parse(this.responseText);
@@ -263,11 +214,9 @@
                                         <div>${assets[i]['AssetID']}</div>
                                         <div>${assets[i]['assetName']}</div>
                                         <div>${assets[i]['assetType']}</div>
-
                                         <div>  
                                         <p><button id='reportbtn' onClick="report(${assets[i]['AssetID']})">Report</button></p>
                                         </div> 
-
                                     </div>`;
                 }
             }
@@ -275,13 +224,8 @@
         xhr.send();
     }
     loadAssets();
-
-
-
-
     // const xhr = new XMLHttpRequest();
     // xhr.open("GET", "../model/AssignedAssetsEmp.php?action=getAssets", true);
-
     // // xhr.onload = function() {
     //     if (this.status === 200) {
     //         var assets = JSON.parse(this.responseText);
@@ -293,21 +237,15 @@
     //                                 <div>${assets[i]['AssetID']}</div>
     //                                 <div>${assets[i]['assetName']}</div>
     //                                 <div>${assets[i]['assetType']}</div>
-
     //                                 <div>  
     //                                 <p><button id='reportbtn' onClick="report(${assets[i]['AssetID']})">Report</button></p>
     //                                 </div> 
-
     //                             </div>`;
     //         }
     //     }
     // }
-
     //     // xhr.setRequestHeader("Content-type", "application/json");
     //     xhr.send();
-
-
-
         
     function report(asset){
         var assetDetails=null;
@@ -321,32 +259,24 @@
             }                
         }
         xhr.send();
-
         console.log(asset);
        
      }
-
       
-
     //    function test(assetDetails){
     //     console.log(assetDetails);
-
     //     loadSection('centerSection','report');
-
              
     //     var testone = document.getElementById('assetID');
     //     testone.value= assetDetails['AssetID'];   //assetDEtails to assetDetails
     //      console.log();
-
            
     //    }
     
-
     
         //console.log(asset);
      
    // }
-
 </script>
 
 
@@ -359,7 +289,6 @@
 <!-- <script>
     var reportbtn = document.getElementById('reportbtn');
     reportbtn.addEventListener('click',function(){
-
         console.log('looooooaddd plzz');
         loadSection('centerSection','report');
         
