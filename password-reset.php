05af452664c407cdcb0e245970eaed4ffb62a5e4
IndunJD <indunijd@gmail.com>
2021-10-19 09:45:27 +0530

    forgot password all complete

--- password-reset.php
+++ password-reset.php
@@ -114,15 +114,20 @@
             color: #EAEDF5;
             cursor: pointer;
         }
+        .successAlert {
+            z-index: 10;
+            color: white;
+            text-align: center;
+        }
     </style>
 </head>
 
 <body>
-
+    <!-- PLace to show the success message -->
     <?php
         if(isset($_SESSION['status'])) {
             ?>
-            <div class="success alert">
+            <div class="successAlert">
                 <h5><?= $_SESSION['status']; ?></h5>
             </div>
             <?php
