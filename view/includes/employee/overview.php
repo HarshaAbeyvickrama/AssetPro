ae3af8033deb3832547c6b76fecbfdf3710a8988
Mushrifa <mushimmf7877@gmail.com>
2021-10-20 17:30:23 +0530

    mod overview chnge

--- view/includes/employee/overview.php
+++ view/includes/employee/overview.php
@@ -2,11 +2,7 @@
     .overviewLayout{
         display: grid;
         grid-template-rows: 0.75fr 1.5fr 0.75fr 7fr ;
-        /* height: 82vh;
-        width: 87.5vw;
-        overflow-y: auto;
-        padding: 20px;
-        background-color: #F1F4FF; */
+             
     }
     .overviewLayout > div{
         display: grid;
@@ -14,11 +10,10 @@
         color: #304068;
         font-size: 24px;
         font-weight: bold;
-        
     }
     .statSection{
         display: grid;
-        grid-template-columns: repeat(5 1fr);
+        grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
         width: 100%;
         height: 100%;
     }
@@ -26,6 +21,7 @@
         width: 100%;
         height: 100%;
         display: flex;
+        /* justify-content: center; */
         align-items: center;
     }
     .statBox{
@@ -63,12 +59,8 @@
     .box5{
         background-color: #4E74AB;
     }
-    /* .contentSection{
-        background-color: white;
-        border-radius: 15px;
-        padding:15px;
-    } */
 
+    
     .overviewLayout .contentSection{
         all: revert;
         display: inline-block;
@@ -88,25 +80,43 @@
         font-weight: bold;
     }
 
-    /*............Recent Activities CSS..............*/
-    
 
-
-
-
-
-/*inside content section*/
-    .econtainer1{     
-        width:100%;
-        height:100%;
-        display:grid;
-        grid-template-columns:1fr 1fr 2fr 1fr;
-        font-size:18px;
-        font-weight:lighter;
-        text-align:center;      
+    /* Recent activity Table CSS */
+    .table{
+        display: table;
+        width: 100%;
+        margin: 10px 0px;
+        
     }
-   
-   
+    .tableHeader{
+        display: table-header-group;
+        font-size: 19px;
+        padding: 15px;
+        font-weight: bold;
+    }
+    .table .tableRowGroup{
+        display: table-row-group;
+    }
+    .tableRow{
+        display: table-row;
+    }
+    .tableCell{
+        display: table-cell;
+    }
+    .tableRowGroup .tableRow:hover{
+        cursor: pointer;
+        background-color: grey;
+        
+    }
+    .tableRow .tableCell{
+        padding:10px 0px;
+        
+    }
+    hr{
+        background-color: #304068;
+        width: 100%;
+        /* height: 1px; */
+    }
 </style>
 <div class="overviewLayout">
     <div>
@@ -122,87 +132,158 @@
 
         <div>
             <div class="statBox box2" >
-                    <div class="statNumber" id="Tangibles"></div>
-                    <div class="statText">Tangible Assets</div>
+                    <div class="statNumber" id="allTangibles"></div>
+                    <div class="statText">All Tangibles</div>
             </div>
         </div>
             
         <div>
             <div class="statBox box3" >
-                <div class="statNumber" id="Fixeds">40</div>
-                    <div class="statText">Fixed Assets</div>
+                <div class="statNumber" id="allFixeds"></div>
+                    <div class="statText">All Fixeds</div>
             </div>
         </div>
-
         <div>
-            <div class="statBox box4" >
-                <div class="statNumber" id="Consumables">30</div>
-                <div class="statText">Consumable Assets</div>
+            <div class="statBox box4">
+                <div class="statNumber" id="allConsumables"></div>
+                    <div class="statText">allConsumables</div>
             </div>
         </div>
-
         <div>
-            <div class="statBox box5" >
-                <div class="statNumber" id="Intangibles">30</div>
-                <div class="statText">Intangible Assets</div>
+            <div class="statBox box5">
+                <div class="statNumber"></div>
+                    <div class="statText"></div>
             </div>
         </div>
-
-        
     </div>
-
     <div>
-    <div>Recent Activities</div>
+        <div>Recent Activities</div>
     </div>
+    <div class="contentSection scrollbar">
+        <div class="recent24">
+            <div class="recentTitle">Last 24 Hours</div>
 
-    <div class="contentSection">
-        <div class="econtainer1"> 
-                 <div>
-                     <h5>Date</h5>
-                     <p>2021/9/09</p>
-                     <p>2021/7/07</p>
-                     <p>2021/1/09</p>
-                     <p>2021/1/19</p>
-                     <p>2021/7/09</p>
-                 </div>
-
-                 <div>
-                     <h5>Role</h5>
-                     <p>Employee1</p>
-                     <p>Employee3</p>
-                     <p>Employee7</p>
-                     <p>Employee9</p>
-                     <p>Employee4</p>
-                 </div>
-
-                 <div>
-                     <h5>Task</h5>
-                     <p>Reported breakdown of the asset FA/C/123</p>
-                     <p>Reported breakdown of the asset CA/T/323</p>
-                     <p>Reported breakdown of the asset FA/C/190</p>
-                     <p>Reported breakdown of the asset FA/T/200</p>
-                     <p>Reported breakdown of the asset FA/T/890</p>
-                 </div>
-                 
-                 <div>
-                     <h5>Time</h5>
-                     <p>5 days ago</p>
-                     <p>7 days ago</p>
-                     <p>9 days ago</p>
-                     <p>3 days ago</p>
-                     <p>1 days ago</p>
-                 </div>
+            <div class="recentActivityTable table">
+                <div class="tableHeader">
+                    <div class="tableCell">Date</div>
+                    <div class="tableCell">Name</div>
+                    <div class="tableCell">Task</div>
+                    <div class="tableCell">Role</div>
+                    <div class="tableCell">Time</div>
+                </div>
+                <div class="tableRowGroup">
+                    <div class="tableRow">
+                        <div class="tableCell">18/07/2020</div>
+                        <div class="tableCell">Wathsala</div>
+                        <div class="tableCell">Reported Breakdown: FA/12345</div>
+                        <div class="tableCell">Employee</div>
+                        <div class="tableCell">5 Hours ago</div>
+                    </div>
+                    <div class="tableRow">
+                        <div class="tableCell">18/07/2020</div>
+                        <div class="tableCell">Wathsala</div>
+                        <div class="tableCell">Reported Breakdown: FA/12345</div>
+                        <div class="tableCell">Employee</div>
+                        <div class="tableCell">5 Hours ago</div>
+                    </div>
+                   
+                </div>
+            </div>
         </div>
-        
+
+        <hr>
+
+        <div class="oldActivities">
+            <div class="recentTitle">Earlier</div>
+
+            <div class="recentActivityTable table">
+                <div class="tableHeader">
+                    <div class="tableCell">Date</div>
+                    <div class="tableCell">Name</div>
+                    <div class="tableCell">Task</div>
+                    <div class="tableCell">Role</div>
+                    <div class="tableCell">Time</div>
+                </div>
+                <div class="tableRowGroup">
+                    <div class="tableRow">
+                        <div class="tableCell">18/07/2020</div>
+                        <div class="tableCell">Wathsala</div>
+                        <div class="tableCell">Reported Breakdown: FA/12345</div>
+                        <div class="tableCell">Employee</div>
+                        <div class="tableCell">5 Hours ago</div>
+                    </div>
+                    <div class="tableRow">
+                        <div class="tableCell">18/07/2020</div>
+                        <div class="tableCell">Wathsala</div>
+                        <div class="tableCell">Reported Breakdown: FA/12345</div>
+                        <div class="tableCell">Employee</div>
+                        <div class="tableCell">5 Hours ago</div>
+                    </div>
+                    <div class="tableRow">
+                        <div class="tableCell">18/07/2020</div>
+                        <div class="tableCell">Wathsala</div>
+                        <div class="tableCell">Reported Breakdown: FA/12345</div>
+                        <div class="tableCell">Employee</div>
+                        <div class="tableCell">5 Hours ago</div>
+                    </div>
+                    <div class="tableRow">
+                        <div class="tableCell">18/07/2020</div>
+                        <div class="tableCell">Wathsala</div>
+                        <div class="tableCell">Reported Breakdown: FA/12345</div>
+                        <div class="tableCell">Employee</div>
+                        <div class="tableCell">5 Hours ago</div>
+                    </div>
+                    <div class="tableRow">
+                        <div class="tableCell">18/07/2020</div>
+                        <div class="tableCell">Wathsala</div>
+                        <div class="tableCell">Reported Breakdown: FA/12345</div>
+                        <div class="tableCell">Employee</div>
+                        <div class="tableCell">5 Hours ago</div>
+                    </div>
+                    <div class="tableRow">
+                        <div class="tableCell">18/07/2020</div>
+                        <div class="tableCell">Wathsala</div>
+                        <div class="tableCell">Reported Breakdown: FA/12345</div>
+                        <div class="tableCell">Employee</div>
+                        <div class="tableCell">5 Hours ago</div>
+                    </div>
+                    <div class="tableRow">
+                        <div class="tableCell">18/07/2020</div>
+                        <div class="tableCell">Wathsala</div>
+                        <div class="tableCell">Reported Breakdown: FA/12345</div>
+                        <div class="tableCell">Employee</div>
+                        <div class="tableCell">5 Hours ago</div>
+                    </div>
+                    <div class="tableRow">
+                        <div class="tableCell">18/07/2020</div>
+                        <div class="tableCell">Wathsala</div>
+                        <div class="tableCell">Reported Breakdown: FA/12345</div>
+                        <div class="tableCell">Employee</div>
+                        <div class="tableCell">5 Hours ago</div>
+                    </div>
+                    <div class="tableRow">
+                        <div class="tableCell">18/07/2020</div>
+                        <div class="tableCell">Wathsala</div>
+                        <div class="tableCell">Reported Breakdown: FA/12345</div>
+                        <div class="tableCell">Employee</div>
+                        <div class="tableCell">5 Hours ago</div>
+                    </div>
+                    <div class="tableRow">
+                        <div class="tableCell">18/07/2020</div>
+                        <div class="tableCell">Wathsala</div>
+                        <div class="tableCell">Reported Breakdown: FA/12345</div>
+                        <div class="tableCell">Employee</div>
+                        <div class="tableCell">5 Hours ago</div>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
-<script>
-    // document.getElementById('allAssets').innerHTML =  getCount('allAssets');
-    getCount('allAssets','allAssets');
-    getCount('allTangible','Tangibles');
-    getCount('allFixed','Fixeds');
-    getCount('allConsumables','Consumables');
-    getCount('allIntangibles','Intangibles');
-    // console.log('c : ',a);
-</script>
+<!-- <script>
+    getCount('allAssets','allAssetsCount');
+    getCount('allEmployees','allEmployeesCount');
+    getCount('allTechnicians','allTechniciansCount');
+</script> -->
