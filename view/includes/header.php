ecf9eed22243bd41d9f0a4a63305767ef78f4c0f
IndunJD <indunijd@gmail.com>
2021-10-21 21:10:03 +0530

    css for drop down

--- view/includes/header.php
+++ view/includes/header.php
@@ -78,7 +78,7 @@
         box-shadow: 0 0px 2px #5c6e9b;
         z-index: 10;
     }
-    .profile-dropdown > div{
+    /* .profile-dropdown > div{
         padding: 0px 10px;
     }
     .profile-dropdown > hr{
@@ -88,6 +88,65 @@
     #logout{
        justify-content: left;
     }
+    .topic:hover, .log-out:hover {
+        color: #5c6e9b;
+        background-color: #EAEDF5;
+        cursor: pointer;
+    } */
+    .profile-dropdown .menu {
+        position: absolute;
+        top: 80px;
+        right: -10px;
+        padding: 10px 20px;
+        background: #FAFBFF;
+        width: 200px;
+        box-sizing: 0 5px 25px rgba(0,0,0,0.1);
+        border-radius: 15px;
+        transition: 0.5s;
+    }
+
+    .profile-dropdown .menu::before {
+        content: '';
+        position: absolute;
+        top: -5px;
+        right: 28px;
+        width: 20px;
+        height: 20px;
+        background: #FAFBFF;
+        transform: rotate(45deg);
+    }
+    .profile-dropdown .menu h3 {
+        width: 100%;
+        text-align: center;
+        font-size: 18px;
+        padding: 20px 0;
+        font-weight: 500;
+        color: #304068;
+        line-height: 1.2em;
+    }
+    .profile-dropdown .menu h3 span {
+        font-size: 14px;
+        color: #5c6e9b;
+        font-weight: 400;
+    }
+    .profile-dropdown .menu ul li {
+        list-style: none;
+        padding: 15px 0;
+        border-top: 1px solid rgba(0,0,0,0.05);
+        display: flex;
+        align-items: center;
+        color: #5c6e9b;
+    }
+    .profile-dropdown .menu ul li img {
+        max-width: 20px;
+        margin-right: 10px;
+        opacity: 0.5;
+        transform: 0.5s;
+    }
+    .profile-dropdown .menu ul li:hover img {
+        opacity: 1;
+    }
+
 </style>
 <div class="header">
     <div class="notificationBadge">
@@ -111,18 +170,28 @@
         </div>
     </div>
 </div>
-<div class="profile-dropdown">
-    <div>Signed in as</div>
-    <div>
+<!-- <div class="profile-dropdown">
+    <div class="maintopic">Signed in as</div>
+    <div class="signedName">
         <?php
             echo $name;
         ?>
     </div>
     <hr>
-    <div id="your-profile">
+    <div id="your-profile" class="topic">
         Your Profile
     </div>
-    <div id="logout">
+    <div id="logout" class="log-out">
         Log Out
+    </div>
+</div> -->
+
+<div class="profile-dropdown">
+    <div class="menu">
+        <h3>Signed in as <br> <span> <?php echo $name?> </span></h3>
+        <ul>
+            <li><img src="../Images/avatar.png" alt="">My Profile</li>
+            <li><img src="../Images/settings.jpg" alt="">My Profile</li>
+        </ul>
     </div>
 </div>
