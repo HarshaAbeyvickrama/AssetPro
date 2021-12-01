db71b0d6aac8065c98c45dddec7cfbe0cd66eb7a
IndunJD <indunijd@gmail.com>
2021-12-01 22:22:05 +0530

    login somewhat done

--- model/dbConnection.class.php
+++ model/dbConnection.class.php
@@ -16,5 +16,53 @@ class DBConnection{
             die();
         }
     }
+    //Newly added
+    //Prepare statement with query
+    public function query($sql) {
+        $this->stmt = $this->dbConnection->prepare($sql);
+    }
+
+    //Bind values to prepared statement using named parameters
+    public function bind($param, $value, $type = null) {
+        if(is_null($type)) {
+            switch(true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    //Execute the prepared statement
+    public function execute() {
+        return $this->stmt->execute();
+    }
+
+    //Return multiple records
+    public function resultSet() {
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    //Return  single record
+    public function single() {
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    //Get row count
+    public function rowCount() {
+        return $this->stmt->rowCount();
+    }
 }
+
 ?>
---
+++ view/login.php
@@ -0,0 +1,70 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8" />
+  <title>AssetPro</title>
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="../css/login.css">
+  <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+</head>
+
+<?php
+  //create the helper page and it's functions
+  include_once '../helpers/sessionHelper.php';
+?>
+
+<body>
+  <div class="center">
+
+    <?php flash('login') ?>
+
+    <div class="logo">
+      <img src="../Images/AssetProLogo.png" alt="AssetPro Logo">
+    </div>
+    <form id="loginForm" method="POST" action="../controller/userController.php">
+      <input type="hidden" name="type" value="login">
+      <div class="txt_field">
+        <input type="text" name="Username" autocomplete="off" required />
+        <span></span>
+        <label>Username</label>
+      </div>
+      <div class="txt_field">
+        <input type="password" name="Password" required />
+        <span></span>
+        <label>Password</label>
+      </div>
+      <input type="submit" value="Login" id="btnLogin" />
+      <br>
+      <a href="password-reset.php" class="pass" id="forgotPass">Forgot Password?</a>
+      
+    </form>
+  </div>
+
+</body>
+
+</html>
+<!-- <script>
+  const form = document.getElementById('loginForm');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    var login = new FormData(form);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', './controller/mainController.php?action=login', true);
+    xhr.onload = function() {
+      const res = JSON.parse(this.responseText);
+      if (res.status != "ok") {
+        alert("Wrong userame/password combination!!!!!")
+      } else {
+        // console.log(`http://localhost/assetpro/${res.location}`);
+        window.location.replace(res.location);
+      }
+    }
+    xhr.send(login);
+  })
+</script> -->
