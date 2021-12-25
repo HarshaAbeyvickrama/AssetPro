642ce41fbddfe042cea886d5252510862c9f038f
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2021-12-25 21:36:05 +0530

    methods for disposal

--- model/depriciation.class.php
+++ model/depriciation.class.php
@@ -2,8 +2,7 @@
 class Depriciation extends DBConnection
 {
 
-    function calculate()
-    {
+    function calculate(){
         $sql = "SELECT
                         d.DepreciationID,
                         d.AssetID,
@@ -22,5 +21,46 @@ class Depriciation extends DBConnection
         $stmt->bindParam(':assetID', $assetID);
         $stmt->execute();
         return $stmt->fetch()['cost'];
+    }
+
+    function updateDepriciation($assetID){
+        $sql = "UPDATE depreciation SET isDepriciated = true WHERE AssetID = :assetID";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->bindParam(':assetID', $assetID);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    function getAllDepriciated(){
+        $sql = "SELECT
+                    d.DepreciationID,
+                    d.AssetID,
+                    d.UsefulYears,
+                    ad.Cost
+                FROM
+                    `depreciation` d
+                INNER JOIN assetdetails ad ON
+                    d.AssetID = ad.AssetID
+                WHERE
+                    d.isDepriciated = TRUE" ;
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    function submit($assetID){
+        $sql = "UPDATE depreciation SET isDisposed = true WHERE AssetID = :assetID";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->bindParam(':assetID', $assetID);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    function isDisposed($assetID){
+        $sql = "SELECT isDisposed FROM depreciation WHERE AssetID = :assetID";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->bindParam(':assetID', $assetID);
+        $stmt->execute();
+        return $stmt->fetch()['isDisposed'];
     }
 }
